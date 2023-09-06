852119829948b060ae082e2184f1e77b291550a9
Colby Garland <colbygarland@gmail.com>
2023-09-05 21:09:21 -0600

    add firebase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,12 @@
+import { initFirebase } from '@/firebase'
 import '@/styles/globals.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
+
+initFirebase()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
